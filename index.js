72e828dcfa7e87cf1db8468acfa20c6b329e0ed3
Ravi <ravivyas922@gmail.com>
2023-01-10 18:51:03 -0500

    commit tree f26afedd4b9b54d1070252694feec5f91c922cda

--- index.js
+++ index.js
@@ -8,7 +8,53 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(array)
+{
+    const employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    };
+return employee;
+}
+function createEmployeeRecords(arrayOffArrays)
+{
+    const employeeRecords = [];
+    for (const arr of arrayOffArrays)
+    {
+        employeeRecords.push(createEmployeeRecord(arr));
+    }
+    return employeeRecords;
+}
+function createTimeInEvent(date)
+{
+const dateArray = date.split(" ");
+const dateObject = 
+{
+    type: "TimeIn",
+    hour: dateArray[1],
+    date: dateArray[0]
+}
+console.log(dateObject);
+}
+function createTimeOutEvent(date)
+{
+const dateArray = date.split(" ");
+const dateObject = 
+{
+    type: "TimeOut",
+    hour: dateArray[1],
+    date: dateArray[0]
+}
+console.log(dateObject);
+}
+function hoursWorkedOnDate(hourDate)
+{
 
+}
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
